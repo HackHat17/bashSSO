ab04f651e153cda76032d697760d370d226bb172
HackHat17 <84771225+HackHat17@users.noreply.github.com>
2021-11-20 00:50:10 -0600

    Add files via upload

---
+++ uwu1.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#identifica OS
+	echo "El sistema es: "
+	echo "$UNAME"
+		echo "Detectando ClamAV"
+		Flag=$(dpkg-query -l | grep clamav)
+		if  ( "$Flag" =="" ); 
+		then
+			echo "Instalando ClamAV"
+			apt-get install clamav clamav-daemon -y
+		else 
+			echo "ClamAV ya está instalado "
+		fi
+
+		echo "presione enter para actualizar"
+		read -p "presione cualquier tecla para cancelar" ack
+		if ( -z "$ack" );
+		then
+			echo "buscando actualizaciones"
+			apt-get update && apt-get upgrade -y
+		else 
+			echo "actualización cancelada"
+		fi
