021c73392d202d45fa84990dd695e7e82c010344
HackHat17 <84771225+HackHat17@users.noreply.github.com>
2021-11-20 00:33:10 -0600

    Add files via upload

---
+++ bashff.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+#identifica OS
+echo "Detectando SO"
+UNAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+#imprimir OS
+OS=$(hostnamectl | grep centos)
+
+if [[ -z "$OS" ]];
+then
+
+	echo "El sistema es: "
+	echo "$UNAME"
+		echo "Detectando ClamAV"
+		Flag=$(sudo dpkg-query -l | grep clamav)
+		if  [[ -z "$Flag" ]]; 
+		then
+			echo "Instalando ClamAV"
+			apt-get install clamav clamav-daemon -y
+		else 
+			echo "ClamAV ya está instalado "
+		fi
+
+		echo "presione enter para actualizar"
+		read -p "presione cualquier tecla para cancelar" ack
+		if [[ -z "$ack" ]];
+		then
+			echo "buscando actualizaciones"
+			apt-get update && apt-get upgrade -y
+		else 
+			echo "actualización cancelada"
+		fi
+
+fi 
+OS=$(hostnamectl | grep centos)
+if [[ -z "$OS" ]];
+then
+	echo "El sistema es: "
+	echo "$UNAME"
+	Flag=$(yum list installed | grep epel)
+		if [[ -z "$Flag" ]];
+		then
+			echo "Instalando EPEL"
+			yum -y install epel-release
+			echo "EPEL instalado"
+		else 
+			echo "EPEl ya está instalado"
+		fi
+		echo "Detectando ClamAV"
+		Flag=$(yum list installed | grep clamav)
+		if  [[ -z "$Flag" ]]; 
+		then
+			echo "Instalando ClamAV"
+			yum -y install clamav clamav-devel
+		else 
+			echo "ClamAV ya está instalado "
+		fi
+
+		echo "presione enter para actualizar"
+		read -p "presione cualquier tecla para cancelar" ack
+		if [[ -z "$ack" ]];
+		then
+			echo "buscando actualizaciones"
+			yum update
+		else 
+			echo "actualización cancelada"
+		fi
+fi
+exit
