1dace17c5dd3cfbaabb3d665aab421327719bf14
HackHat17 <84771225+HackHat17@users.noreply.github.com>
2021-11-19 21:41:02 -0600

    Add files via upload

---
+++ bash6.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+#identifica OS
+echo "Detectando SO"
+UNAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+#imprimir OS
+echo "El sistema es: "
+if [ $UNAME == '"CentOS Linux"' ]
+then
+    
+	echo "Instalando EPEL"
+	yum -y install epel-release
+	echo "EPEL instalado"
+	#condicion para revisar si está instalado clamav, si no instalarlo
+	echo "Detectando ClamAV"
+	if  [ yum list -q installed clamav ] 
+	then
+		echo "Instalando ClamAV"
+		yum -y install clamav clamav-devel
+	fi
+#yum update
+fi
+
+if [ $UNAME == '"Ubuntu"' ]
+then
+    echo "$UNAME"
+	echo "Detectando ClamAV"
+	CLAMAV=$(which clamav)
+	echo "$CLAMAV"
+	
+	if  [ -z "$CLAMAV" ] 
+	then
+		echo "Instalando ClamAV"
+		#apt install clamav
+	fi
+	echo "actualizando"
+    #apt-get update
+fi
+
+echo "fin del script"
+exit
