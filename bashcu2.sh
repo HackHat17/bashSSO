950faa74a4a7dce5ac526d3843fbdb5aed388fec
HackHat17 <84771225+HackHat17@users.noreply.github.com>
2021-11-19 23:45:07 -0600

    Add files via upload

---
+++ bashcu2.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+#identifica OS
+echo "Detectando SO"
+UNAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+#imprimir OS
+centos='"CentOS Linux"'
+ubuntu='"Ubuntu"'
+
+if [["$UNAME" == "$ubuntu"]];
+then
+echo "es ubuntu"
+
+else 
+	echo "El sistema es: "
+	echo "$UNAME"
+	Flag=$(yum list installed | grep epel)
+		if [[ -z "$Flag" ]];
+		then
+			echo "Instalando EPEL"
+			yum -y install epel-release
+			echo "EPEL instalado"
+		else 
+			echo "EPEl ya está instalado"
+		fi
+		echo "Detectando ClamAV"
+		Flag=$(yum list installed | grep epel)
+		if  [[ -z "$Flag" ]]; 
+		then
+			echo "Instalando ClamAV"
+			yum -y install clamav clamav-devel
+		else 
+			echo "ClamAV ya está instalado "
+		fi
+
+		read -p "¿deseas actualizar? (y/n)" ack
+		if [["$ack" == "y"]];
+		then
+			echo "buscando actualizaciones"
+			yum update
+		else 
+			echo "actualización cancelada"
+		fi
+
+
+fi
+
+
+
+exit
