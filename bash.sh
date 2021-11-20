d00a94b75387371ee225f29acee08f214bff4c92
HackHat17 <84771225+HackHat17@users.noreply.github.com>
2021-11-19 19:44:35 -0600

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+#identifica OS
+echo "Detectando SO"
+
+UNAME=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+
+#imprimir OS
+echo "El sistema es: "
+echo "$UNAME"
+
+#condicional de SO
+
+echo "Instalando EPEL"
+yum -y install epel-release
+echo "EPEL instalado"
+
+#condicion para revisar si está instalado clamav, si no instalarlo
+
+echo "Detectando ClamAV"
+if  [um list installed | grep clamav = ""]; then 
+
+	echo "Instalando ClamAV"
+
+	
+	yum -y install clamav clamav-devel
+
+	else
+
+	echo "clamAV ya se encuentra instalado"
+
+	fi
+fi
+
+yum update
+
+echo "fin del script"
